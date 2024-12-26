511984ebf76ba5ec8bd4379369c539d8ce611cc8
Manish <manish@Manishs-Air.attlocal.net>
2024-12-26 12:40:12 -0600

    ISUW Home Pag eChange

--- resources/views/components/home.blade.php
+++ resources/views/components/home.blade.php
@@ -137,15 +137,15 @@
                            </div>
                         </div>
                      </div>
-                     <h2 class="mt-4">Key Partners 2025
+                     <h2 class="mt-4">Key Partner 2025
                      </h2>
                      <div class="row justify-content-center">
                         <div class="col-lg-12">
                            <div class="logo-title">
-                              <h4>Platinum Partners </h4>
+                              <h4>Platinum Partner </h4>
                            </div>
                         </div>
-                        <div class="col-lg-4">
+                        <div class="col-lg-5">
                            <a href="https://www.gevernova.com/" target="_blank" rel="noopener noreferrer">
                               <div class="logo-div">
                                  <img src="https://www.isuw.in/public/uploads/partner/GE_Vernova_Standard_RGB_Evergreen.jpg" alt="" style="display:inline; height:50px">
@@ -157,10 +157,10 @@
                      <div class="row justify-content-center">
                         <div class="col-lg-12">
                            <div class="logo-title">
-                              <h4>Bronze Partners </h4>
+                              <h4>Bronze Partner </h4>
                            </div>
                         </div>
-                        <div class="col-lg-4">
+                        <div class="col-lg-5">
                            <a href="https://edx.com/" target="_blank" rel="noopener noreferrer">
                               <div class="logo-div">
                                  <img src="images/2025/edxWireless.jpeg" alt="" style="display:inline; height:50px">
