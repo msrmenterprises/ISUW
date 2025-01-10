cf8cc0a8637acaa9e1a2933f501e7e7ae6a2435f
Manish <manish@Manishs-Air.attlocal.net>
2025-01-09 20:30:25 -0600

    Conference Page Update IWT-52

--- resources/views/components/conference-themes.blade.php
+++ resources/views/components/conference-themes.blade.php
@@ -32,27 +32,25 @@
   <div class="accordion-item">
     <h2 class="accordion-header" id="flush-headingOne">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-         <H4 style="color: #ed7d31;"><strong>12 March 2024 (Tuesday) - Event Day 1  </strong></style></H4>
+         <H4 style="color: #ed7d31;"><strong>EVENT DAY-1: 18 MARCH 2025 (TUESDAY)</strong></style></H4>
       </button>
     </h2> 
     <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
       <div class="accordion-body">
-     <p style="color:#ed7d31;">Master Class
-     <ul><li>Master Class on Energy Transition Strategies and Pathways to Net Zero Power
-Systems</li>
+     <p style="color:#ed7d31;">MASTER CLASSES
+     <ul><li>Digitalization Of Utilities</li>
 <!--<li>Introductory Course on Transformers and Generative AI: OpenAI GPT-3, ChatGPT and AI
 Tools for Text 2 Text (T2T), Audio 2 Text (A2T), Text 2 Audio (T2A), Text 2 Image (T2I), Image
 2 Text (I2T), Text 2 Video (T2V), Text 2 Motion (T2M), Text 2 Code (T2C), Brain 2 Text (B2T),
 Audio 2 Audio (A2A) etc.</li>
 <li>OCPP Technical Workshop and Training (in collaboration with Open Charge Alliance)</li>--></ul></p>
- <p style="color:#ed7d31;">Special Workshops
-     <ul><li>OCPP Technical Workshop and Training (In Collaboration with Open Charge
-Alliance)</li> 
-<li>Experiential Workshop on Immersive Technologies – Augmented Reality, Virtual
-Reality, Mixed Reality, Assisted Reality, Metaverse etc</li>
-<li>Workshop on 5G Private Network for Critical Infrastructure</li>
-<li>8th IEC - IEEE Smart Energy Standardization Coordination Workshop (In
-collaboration with BIS, IEC and IEEE)</li>
+ <p style="color:#ed7d31;">SPECIAL WORKSHOPS
+     <ul><li>OCPP Testival (In Collaboration with Open Charge Alliance)</li> 
+<li>Short List of Climate Actions That Will Work (In Collaboration With "The Future Is
+Electric")</li>
+<li>9th IEC - IEEE Smart Energy Standardization Coordination Workshop (In
+Collaboration with BIS, IEC And IEEE)</li>
+
 </ul></p>
 </div></div></div></div>
 <p></p>
@@ -60,27 +58,25 @@ collaboration with BIS, IEC and IEEE)</li>
   <div class="accordion-item">
     <h2 class="accordion-header" id="flush-headingOne">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapsetwo" aria-expanded="false" aria-controls="flush-collapsetwo">
-         <H4 style="color: #ed7d31;"><strong>13th March 2024 ( Wednesday ) – Event Day 2</strong></style></H4>
+         <H4 style="color: #ed7d31;"><strong>EVENT DAY- 2: 19 MARCH 2025 (WEDNESDAY)</strong></style></H4>
       </button>
     </h2>
     <div id="flush-collapsetwo" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
       <div class="accordion-body">
- <p style="color:#ed7d31;">Conference Thematic Session
- <ul><li>Digitalization of Utilities, Digitalization Roadmaps; Digital Twins</li>
- <li>Emerging Technologies for Utilities (in Partnership with Department of Science and
-Technology*)</li></ul>
+ <p style="color:#ed7d31;">CONFERENCE THEMATIC SESSIONS
+ <ul><li>Special Plenary Keynotes</li>
+ <li>250 Million Smart Meters in India – New Business Opportunities with the
+ Digitalization of DISCOMs</li>
+<li>Resiliency of Utilities Against Extreme Weather Events</li>
+<li>RD&D of Emerging/Digital Technologies for Utilities (in collaboration with DST)*</li>
+</ul>
  </p>
 
- <p style="color:#ed7d31;">Workshops / Roundatables
-     <ul><li>13th EU - India Smart Energy Workshop (in Collaboration with European Union*)</li>
-     <li>1st India - Brazil Smart Energy Workshop (In Collaboration with LATAM Smart Grid
-Forum and Brazil-India Chamber of Commerce*) Themes: T&amp;D Loss Reduction and
-Biofuels</li>
-     <li>10th US – India Smart Energy Workshop (In Collaboration with US DOC,
-USAID, USTDA and CSIS*) Theme: Resource Adequacy Planning for Net
-Zero Power Systems</li>
-     <li>SPECIAL PLENARY 1: Energy Transition Pathways for India by <b>Michael Barnard</b>,
-Founder of <b>“The Future is Electric”</b></li>
+ <p style="color:#ed7d31;">WORKSHOPS/ROUNDTABLES/SEMINARS
+     <ul><li>14th EU-INDIA Smart Energy Workshop</li>
+     <li>2nd BRAZIL-INDIA Smart Energy Workshop</li>
+     <li>Deep Dive Workshop on AI/ML Use Cases for Utilities</li>
+     
      </ul></p>
 
 </div></div></div></div>
@@ -89,37 +85,26 @@ Founder of <b>“The Future is Electric”</b></li>
   <div class="accordion-item">
     <h2 class="accordion-header" id="flush-headingOne">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapsethree" aria-expanded="false" aria-controls="flush-collapsethree">
-         <H4 style="color: #ed7d31;"><strong>14th March 2024 ( Thursday ) – Event Day 3</strong></style></H4>
+         <H4 style="color: #ed7d31;"><strong>EVENT DAY- 3: 20 MARCH 2025 (THURSDAY)</strong></style></H4>
       </button>
     </h2>
     <div id="flush-collapsethree" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
       <div class="accordion-body">
-           <p style="color:#ed7d31;">Conference Thematic Session
-    <ul><li>Smart Metering Lessons from Europe and Americas - session with AMISPs and
-DISCOMs (In Collaboration with REC*, PFC*, SAREP and USAID)</li>
-<li>Energy Positive Smart Buildings and Campuses (in Collaboration with
-USAID/SAREP)</li>
-<li>SPECIAL PLENARY 2: Architecture and Energy Systems for Colonizing the
-Moon and the Mars</li>
-<li>Nuclear Renaissance and the Role of SMR in Net Zero Power Systems (In
-Collaboration with NEI, EDF, BARC/NPCIL and NTPC*)</li>
-<li>Climate Resilience of Future Grids (In Collaboration with CDRI*)</li>
-<li>Super Grids for Energy Transition and Energy Security - Cross Border
-Electricity Trade in South Asia and Interconnection of Regional Grids in Asia
-(In Collaboration with GO15 and Hitachi Energy)</li>
+           <p style="color:#ed7d31;">CONFERENCE THEMATIC SESSIONS
+    <ul><li>Rooftop PV Revolution in India and Impact on DISCOMs</li>
+<li>DERMS for Management of Power System Flexibility</li>
+<li>Electric Mobility – Evolving Trends</li>
+<li>Nuclear Renaissance and the Role of Nuclear Energy for the Net Zero Power
+Systems</li>
+<li>Unified Energy Interface (UEI) and the Evolving Era of Energy Internet</li>
+<li>National Registry of Solar Rooftops and Smart Inverters</li>
 </ul></p>
- <p style="color:#ed7d31;">Workshops
-     <ul><li>3rd India - Germany Smart Energy Workshop (In Collaboration with GIZ) Theme:
-Demand Response</li>
-<li>UK FCDO ASPIRE Session</li>
-<li>Virtual Power Plants (VPPs) and Power System Flexibility</li>
-<li>The Cooling Challenge – Cooling as a Service against Monthly Fee through District
-Cooling Systems (DCS) (In Collaboration with BEE and APEUA*)</li>
-<li>Climate Resilience of Future Grids</li>
-<li>Technical Papers Presentation (Selected through Call for Technical Papers @ISUW
-2024)</li>
-<!--<li>Super Grids for Energy Transition and Energy Security - Cross Border Electricity
-Trade in South Asia and Interconnection of Regional Grids in Asia</li>-->
+ <p style="color:#ed7d31;">WORKSHOPS/ROUNDTABLES/SEMINARS
+     <ul><li>US-INDIA Smart Energy Workshop</li>
+<li>GERMANY-INDIA Smart Energy Workshop</li>
+<li>Utilities Integration Seminar</li>
+<li>The Cooling Challenge and the Need for District Cooling Systems (DCS)</li>
+
 </ul></p>
 
 </div></div></div></div>
@@ -128,57 +113,56 @@ Trade in South Asia and Interconnection of Regional Grids in Asia</li>-->
   <div class="accordion-item">
     <h2 class="accordion-header" id="flush-headingOne">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapsefive" aria-expanded="false" aria-controls="flush-collapsefive">
-         <H4 style="color: #ed7d31;"><strong>15 March 2024 (Friday) - Event Day 4</strong></style></H4>
+         <H4 style="color: #ed7d31;"><strong>EVENT DAY- 4: 21 MARCH 2025 (FRIDAY)</strong></style></H4>
       </button>
     </h2>
     <div id="flush-collapsefive" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
       <div class="accordion-body">
-           <p style="color:#ed7d31;">Conference Thematic Session
-    <ul><li>Long Duration Energy Storage Systems (LDES)</li>
-     <li>Policy and Regulatory Support for Fast Tracking the Transition to Net Zero Power
-Systems</li>
-<li>Evolving Trends in Electric Mobility:</li>
- - Electric Trucks<br/>
-- School Bus Electrification<br/>
-- MW-scale Charging Systems for Heavy Vehicles/Planes/Boats and Machinery<br/>
-- V2X Systems<br/>
-- Electric Mobility and Air Quality<br/>
-<li>Digital Tools for DER Management</li>
+           <p style="color:#ed7d31;">CONFERENCE THEMATIC SESSIONS
+    <ul><li>Regulatory and Policy Innovations for the Next Era of Power Sector Reforms</li>
+     <li>Live Line Maintenance in DISCOMs</li>
+<li>Grid Integrated Buildings</li>
+<li>Cyber Security for the Digitalized Green Grid</li>
+<li>Evolving Trends in Electric Mobility</li>
+<li>Role of AI and Robotics in Energy Transition</li>
+<li>Immersive Technologies for Energy Transition</li>
+<li>Carbon Markets</li>
 </ul></p>
- <p style="color:#ed7d31;">Workshops
-     <ul><li>Smart Utilities Integration - Electricity + City Gas + Water</li>
-     <li>Power Markets and Carbon Markets (In Collaboration with CERC, BEE and IEX*)</li>
-     <li>Evolving Trends in Utility Automation (In Collaboration with C-DAC and NaMPET*)</li>
-     <li>Special Session on Role of Media in Promotion of Green Energy and Energy
-Transition towards Net Zero</li>
+ <p style="color:#ed7d31;">WORKSHOPS/ROUNDTABLES/SEMINARS
+     <ul><li>V2G Demo in India</li>
+     <li>Decarbonization of Agriculture Sector</li>
+     <li>Role of Media (and Social Media) in Energy Transition</li>
+     <li>Capacity Building in Utilities for Energy Transition</li>
      </ul></p>
 <p></p>
-<p><b>ISGF Innovation Awards Ceremony And Gala Dinner</b></p>
+<!-- <p><b>ISGF Innovation Awards Ceremony And Gala Dinner</b></p> -->
 </div></div></div></div>
 <p></p>
 <div class="accordion accordion-flush" id="accordionFlushExample">
   <div class="accordion-item">
     <h2 class="accordion-header" id="flush-headingOne">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapsesix" aria-expanded="false" aria-controls="flush-collapsesix">
-         <H4 style="color: #ed7d31;"><strong>16th March 2024 ( Friday ) – Event Day 5</strong></style></H4>
+         <H4 style="color: #ed7d31;"><strong>EVENT DAY- 5: 22 MARCH 2025 (SATURDAY)</strong></style></H4>
       </button>
     </h2>
     <div id="flush-collapsesix" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
       <div class="accordion-body">
 <p></p>
-    <p style="color:#ed7d31;">Technical Tours</p>
- <ul><li>Tour 1 - NRLDC+REMC and TPDDL LAB + 10 MW BESS</li>
- <li>Tour 2 - TPDDL LAB + 10 MW BESS and NRLDC+REMC</li>
- <li>Tour 3 - 800 kV HVDC STATION IN AGRA + TAJ MAHAL</li>
+    <p style="color:#ed7d31;">DEMONSTRATIONS, TECHNICAL AND CULTURAL TOURS</p>
+ <ul><li>TOUR 1 - NRLDC+REMC and TPDDL LAB + 10 MW BESS</li>
+ <li>TOUR 2 - TPDDL LAB + 10 MW BESS and NRLDC+REMC</li>
+ <li>TOUR 3 - 800 kV HVDC STATION IN AGRA + TAJ MAHAL</li>
+ <li>TECHNICAL DEMONSTRATION - Hot Line Maintenance on 11 kV and LT Lines (In
+ collaboration with TPDDL and Altec) *</li>
 </ul>
- <p style="color:#ed7d31;">Cultural Tours</p>
- <ul><li>Delhi</li>
- <li>Taj Mahal</li>
+ <p style="color:#ed7d31;">CULTURAL TOURS (ALL DAY)</p>
+ <ul><li>TOUR 4 – TAJ MAHAL</li>
+ <li>TOUR 5 - DELHI</li>
 </ul>
-<p></p>
+<!-- <p></p>
 <p><b>Disclaimer : Tours 1, 2 &amp; 3 are part of ISUW Package; Tours 4&amp;5 are to be paid separately to the
 Travel Agency | for booking technical &amp; Cultural tours visit the registration desk at Hotel during
-#ISUW24</b></p>
+#ISUW24</b></p> -->
 <p></p>
 <p></p>
 </div></div></div></div>
