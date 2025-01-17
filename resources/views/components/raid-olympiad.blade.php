f125acc90f4a5784a9bbb2fa8b2cef5700763a14
Manish <manish@Manishs-Air.attlocal.net>
2025-01-16 23:47:57 -0600

    IWT64

--- resources/views/components/raid-olympiad.blade.php
+++ resources/views/components/raid-olympiad.blade.php
@@ -31,28 +31,28 @@
             <img src="/public/uploads/images/kborder_bottom.png" alt="">
         </div>
         <div>
-            <h4>The Robots-AI-Drone (RAID) Olympiad 2025</h4>
-            <p>Spring Edition - Seeds of Innovation</p>
-            <p>The Robots-AI-Drone (RAID) Olympiad is a prestigious global initiative by Ideas Matter Most (IMM) that seeks to ignite innovation and foster creativity in the dynamic fields of robotics, artificial intelligence, and drone technologies.</p>
-            <p>The 2025 Spring edition, with a special focus on the Utility Sector, is a collaborative effort between IMM and the India Smart Grid Forum (ISGF). This Olympiad challenges participants to develop groundbreaking solutions addressing key utility sector challenges, such as:</p>
-            <ul>
+            <h4><strong>The Robots-AI-Drone (RAID) Olympiad 2025</strong></h4>
+            <p><b>Spring Edition - Seeds of Innovation</b></p>
+            <p>The Robots-AI-Drone (RAID) Olympiad is a prestigious global initiative by <b>Ideas Matter Most (IMM)</b> that seeks to ignite innovation and foster creativity in the dynamic fields of robotics, artificial intelligence, and drone technologies.</p>
+            <p>The <b>2025 Spring edition</b>, with a special focus on the <b>Utility Sector</b>, is a collaborative effort between IMM and the <b>India Smart Grid Forum (ISGF)</b>. This Olympiad challenges participants to develop groundbreaking solutions addressing key utility sector challenges, such as:</p>
+            <ul class="theme">
                 <li>Infrastructure inspection and maintenance</li>
                 <li>Energy efficiency</li>
                 <li>Smart management systems</li>
             </ul>
             <p>A distinguished panel of experts from India and around the globe will evaluate and judge the competition entries across various phases, ensuring a fair and rigorous assessment process.</p>
-            <h4>Rewards and Opportunities for Participants</h4>
+            <h4><b>Rewards and Opportunities for Participants</b></h4>
             <p>Winning teams, runners-up, and notable participants will receive:</p>
-            <ul>
+            <ul class="theme">
                 <li>Global recognition for their innovative contributions.</li>
                 <li>Exciting awards celebrating their achievements.</li>
-                <li>An exclusive invitation to appear on the Ideas Matter Most Talk Show, in collaboration with Hindustan Times.</li>
+                <li>An exclusive invitation to appear on the <b>Ideas Matter Most Talk Show</b>, in collaboration with Hindustan Times.</li>
                 <li>Mentorship and ongoing support to advance their ideas and careers.</li>
             </ul>
         </div>
         <div class="k_cheading">
-            <a href="#" class="getstarted scrollto">DOWNLOAD BROCHURE</a>
-            <a href="#" class="getstarted scrollto">REGISTER NOW</a>
+            <a href="#" class="getstartedd">DOWNLOAD BROCHURE</a>
+            <a href="#" class="getstartedd">REGISTER NOW</a>
         </div>
     </div>
 </div>
