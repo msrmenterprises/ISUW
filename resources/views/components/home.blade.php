eb8f7551fd7828b5a9cffc888d19660d71a9d064
Manish <manish@Manishs-Air.attlocal.net>
2025-03-02 15:43:18 -0600

    IWT-121

--- resources/views/components/home.blade.php
+++ resources/views/components/home.blade.php
@@ -913,7 +913,7 @@
 			     <div class="col-lg-12 mb-4 text-end">
                   <a class="btn btn-light" style="
     margin-top: -25px;
-" href="/speakers">View All</a>
+" href="/speakers" target="_blank">View All</a>
                </div>
             </div>
             <div class="row justify-content-center ">
