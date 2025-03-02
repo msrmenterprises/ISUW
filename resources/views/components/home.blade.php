58a6ae5e29769e8682419ae459407b6ee310ca1c
Manish <manish@Manishs-Air.attlocal.net>
2025-03-02 14:56:23 -0600

    H

--- resources/views/components/home.blade.php
+++ resources/views/components/home.blade.php
@@ -163,15 +163,15 @@
                               <h4>Host Utility</h4>
                            </div>
                         </div>
-                        <div class="col-lg-4"></div>
-                        <div class="col-lg-4">
+                        
+                        <div class="col-lg-5">
                            <div class="logo-div">
                               <a href="ttps://www.tatapower.com" target="_blank" rel="noopener noreferrer">
                                  <img src="https://www.isuw.in/public/uploads/utility/1641110147.png" alt="" style="display:inline; height:50px">
                               </a>
                            </div>   
                         </div>
-                        <div class="col-lg-4"></div>
+                       
                         
                      </div>
                      <div class="row justify-content-center">
