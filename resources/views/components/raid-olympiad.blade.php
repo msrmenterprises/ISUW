e72368e571182c6da21a9ca5c8311e8d19da973c
Manish <manish@Manishs-Air.attlocal.net>
2025-03-02 21:46:36 -0600

    IWT-118

--- resources/views/components/raid-olympiad.blade.php
+++ resources/views/components/raid-olympiad.blade.php
@@ -39,13 +39,15 @@
             </div>
             <p><b>Spring Edition - Seeds of Innovation</b></p>
             <p>The Robots-AI-Drone (RAID) Olympiad is a prestigious global initiative by <b>Ideas Matter Most (IMM)</b> that seeks to ignite innovation and foster creativity in the dynamic fields of robotics, artificial intelligence, and drone technologies.</p>
-            <p>The <b>2025 Spring edition</b>, with a special focus on the <b>Utility Sector</b>, is a collaborative effort between IMM and the <b>India Smart Grid Forum (ISGF)</b>. This Olympiad challenges participants to develop groundbreaking solutions addressing key utility sector challenges, such as:</p>
-            <ul class="theme">
+            <p>The 2025 Spring edition, with a special focus on the Utility Sector, is a collaborative effort between IMM and the India Smart Grid Forum (ISGF). </p>
+            <!-- <ul class="theme">
                 <li>Infrastructure inspection and maintenance</li>
                 <li>Energy efficiency</li>
                 <li>Smart management systems</li>
-            </ul>
+            </ul> -->
+            <p>This Olympiad challenges participants to develop groundbreaking solutions addressing key utility sector challenges, such as: Infrastructure inspection and maintenance, Energy efficiency, Smart management systems.</p>
             <p>A distinguished panel of experts from India and around the globe will evaluate and judge the competition entries across various phases, ensuring a fair and rigorous assessment process.</p>
+            
             <h4><b>Rewards and Opportunities for Participants</b></h4>
             <p>Winning teams, runners-up, and notable participants will receive:</p>
             <ul class="theme">
