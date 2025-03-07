3579d95bea97af566ea8c301313b45d09003d224
Manish <manish@Manishs-Air.attlocal.net>
2025-03-06 23:08:12 -0600

    IWT-141

--- resources/views/components/theme-and-session-partners-2024.blade.php
+++ resources/views/components/theme-and-session-partners-2024.blade.php
@@ -15,7 +15,13 @@
 	
 	<div class="col-lg-4"><div class="logo-div"><a href="https://www.aspentech.com/en" target="_blank" rel="noopener noreferrer"><img src="images/2025/AspenTech.png" alt=""  height="102" style="display:inline;"></div></a></div>
 	<div class="col-lg-4"><div class="logo-div"><a href="#" target="_blank" rel="noopener noreferrer"><img src="images/2025/BSCE.png" alt=""  height="102" style="display:inline;"></div></a></div>
-	<div class="col-lg-4"></div>
+	
+	<div class="col-lg-4"><div class="logo-div"><a href="https://www.ideasmattermost.com/" target="_blank" rel="noopener noreferrer"><img class="aligncenter size-full wp-image-5610" src="images/2025/Ideasmatter.png" alt="TIE"></a></div></div>
+	<div class="col-lg-4"><div class="logo-div"><a href="https://www.iec.ch/homepage" target="_blank" rel="noopener noreferrer"><img class="aligncenter size-full wp-image-5610" src="images/2025/IEC.png" alt="TIE"></a></div></div>
+	<div class="col-lg-4"><div class="logo-div"><a href="https://www.ieee.org/" target="_blank" rel="noopener noreferrer"><img class="aligncenter size-full wp-image-5610" src="images/2025/IEEE.png" alt="TIE"></a></div></div>
+	<div class="col-lg-4"><div class="logo-div"><a href="https://apua-asea.org/en/" target="_blank" rel="noopener noreferrer"><img class="aligncenter size-full wp-image-5610" src="images/2025/APUA.jpg" alt="TIE"></a></div></div>
+	<div class="col-lg-4"><div class="logo-div"><a href="https://openchargealliance.org/" target="_blank" rel="noopener noreferrer"><img class="aligncenter size-full wp-image-5610" src="images/2025/OpenChargeAlliance.jpg" alt="TIE"></a></div></div>
+	<div class="col-lg-4"><div class="logo-div"><a href="https://www.trade.gov/us-commercial-service" target="_blank" rel="noopener noreferrer"><img class="aligncenter size-full wp-image-5610" src="images/2025/UScommercial.png" alt="TIE"></a></div></div>
 	
  <div class="k_cheading">
 		<h1>ISUW 2024 <span class="k_greencolor">Conference Sessions Partners</span> </h1>
