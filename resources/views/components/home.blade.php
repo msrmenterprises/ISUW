15174c5336cc2cb8598f08485e1343091a08a263
Manish <manish@Manishs-Air.attlocal.net>
2025-03-09 17:32:47 -0500

    IWT-146

--- resources/views/components/home.blade.php
+++ resources/views/components/home.blade.php
@@ -362,6 +362,13 @@
                                  <img src="images/2025/ulse.png" alt="" style="display:inline; height:50px">
                                </div>
                            </a>
+                        </div> 
+                        <div class="col-lg-5">
+                           <a href="https://www.iexindia.com/" target="_blank" rel="noopener noreferrer">
+                              <div class="logo-div">
+                                 <img src="images/2025/IEX.png" alt="" style="display:inline; height:50px">
+                               </div>
+                           </a>
                         </div>  
                      </div>   
                      
