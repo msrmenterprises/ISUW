c137e9d41bf1d60f6eff656c110bc5e08b6fa9a1
Manish <manish@Manishs-Air.attlocal.net>
2025-03-31 21:21:19 -0500

    IWT-158 Table Update

--- resources/views/components/isuw-2025-presentations.blade.php
+++ resources/views/components/isuw-2025-presentations.blade.php
@@ -8,23 +8,40 @@
 		<h1>ISUW<span class="k_greencolor"> 2025 Presentations</span> </h1>
 		<img src="/public/uploads/images/kborder_bottom.png" alt=""> 
 	</div>
+<style type="text/css">
+	table {
+	width: 100%;
+	border-collapse: collapse;
+	}
+	th, td {
+	border: 1px solid #000;
+	padding: 8px;
+	text-align: left;
+	}
+	.session-header {
+	background-color: #FFF2CC;
+	font-weight: bold;
+	}
 
+</style>	
 
-	<h2>9TH IEC-IEEE-BIS SMART ENERGY STANDARDIZATION COORDINATION WORKSHOP</h2>
-    <table>
+<h2>9TH IEC-IEEE-BIS SMART ENERGY STANDARDIZATION COORDINATION WORKSHOP</h2>
+<table>
+    <thead>
         <tr>
-            <th style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000">S. No</th>
-            <th style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000">Name</th>
-            <th style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000">Designation</th>
-            <th style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000">Organization</th>
-            <th style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000">Category</th>
-            <th style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000">Presentation/Speech</th>
-            <th style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000">Video Link</th>
-            <th style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000">Photo Link</th>
+            <th>S. No</th>
+            <th>Name</th>
+            <th>Designation</th>
+            <th>Organization</th>
+            <th>Category</th>
+            <th>Presentation/Speech</th>
+            <th>Video Link</th>
+            <th>Photo Link</th>
         </tr>
+    </thead>
+    <tbody>
         <tr class="session-header">
-           
-			<td  colspan=8 align="left" valign=bottom bgcolor="#FFF2CC"><b><font face="Roboto" color="#000000">Inaugural Session</td></tr>
+            <td colspan="8" align="left">Inaugural Session</td>
         </tr>
         <tr>
             <td>1</td><td>Reji Kumar Pillai</td><td>President</td><td>ISGF</td><td>Welcome Address</td><td>Speech Only</td><td>NA</td><td>NA</td>
@@ -35,11 +52,8 @@
         <tr>
             <td>3</td><td>Sreekanth Chandrasekharan</td><td>Senior Director</td><td>IEEE</td><td>Special Address</td><td>Speech Only</td><td>NA</td><td>NA</td>
         </tr>
-        <tr>
-            <td>4</td><td>BA Sawale</td><td>Director General</td><td>Central Power Research Institute</td><td>Special Address</td><td>Speech Only</td><td>NA</td><td>NA</td>
-        </tr>
         <tr class="session-header">
-            <td colspan="8">Session 1: Emerging Smart Energy Technologies and Standardization Landscape</td>
+            <td colspan="8" align="left">Session 1: Emerging Smart Energy Technologies and Standardization Landscape</td>
         </tr>
         <tr>
             <td>5</td><td>Lonneke Drissen</td><td>Director</td><td>Open Charge Alliance</td><td>Speaker</td><td>View/Download</td><td>NA</td><td>NA</td>
@@ -47,43 +61,15 @@
         <tr>
             <td>6</td><td>Neha Agarwal</td><td>Scientist – D / Joint Director</td><td>BIS</td><td>Speaker</td><td>View/Download</td><td>NA</td><td>NA</td>
         </tr>
-        <tr>
-            <td>7</td><td>Amarjeet Kumar</td><td>Founder & CEO</td><td>Comminent Pvt. Ltd.</td><td>Speaker</td><td>View/Download</td><td>NA</td><td>NA</td>
-        </tr>
-        <tr>
-            <td>8</td><td>Ravindra Desai</td><td>Program Manager – Standards</td><td>IEEE</td><td>Speaker</td><td>View/Download</td><td>NA</td><td>NA</td>
-        </tr>
-        <tr>
-            <td>9</td><td>Remi Demerle</td><td>Marketing director</td><td>LoRA ecosystem</td><td>Speaker</td><td>View/Download</td><td>NA</td><td>NA</td>
-        </tr>
-        <tr>
-            <td>10</td><td>Dinesh Sharma</td><td>Director – Standardization, Policy and Regulation</td><td>SESI</td><td>Speaker</td><td>View/Download</td><td>NA</td><td>NA</td>
-        </tr>
         <tr class="session-header">
-            <td colspan="8">Session 2: Future Trends and Challenges</td>
+            <td colspan="8" align="left">Session 2: Future Trends and Challenges</td>
         </tr>
         <tr>
             <td>13</td><td>Subhadip Raychaudhuri</td><td>Head-Engineering, Energy Audit, GIS & AMI Applications</td><td>TPDDL</td><td>Speaker</td><td>View/Download</td><td>NA</td><td>NA</td>
         </tr>
-        <tr>
-            <td>14</td><td>A K Rajput</td><td>Member (Power Systems)</td><td>CEA</td><td>Speaker</td><td>Speech Only</td><td>NA</td><td>NA</td>
-        </tr>
-        <tr>
-            <td>15</td><td>Miguel Martinez</td><td>Senior Business Manager</td><td>UL Solutions</td><td>Speaker</td><td>View/Download</td><td>NA</td><td>NA</td>
-        </tr>
-        <tr>
-            <td>16</td><td>Gopal Nariya</td><td>Vice President</td><td>BRPL</td><td>Speaker</td><td>View/Download</td><td>NA</td><td>NA</td>
-        </tr>
-        <tr>
-            <td>17</td><td>Kishore Narang</td><td>Founder</td><td>Narnix Technolabs Pvt. Ltd.</td><td>Speaker</td><td>Speech Only</td><td>NA</td><td>NA</td>
-        </tr>
-        <tr>
-            <td>18</td><td>Richard Schomberg</td><td>Special Envoy on Smart Electrification</td><td>IEC</td><td>Chair</td><td>Speech Only</td><td>NA</td><td>NA</td>
-        </tr>
-        <tr>
-            <td>19</td><td>N. Murugesan</td><td>Former DG</td><td>CPRI</td><td>Moderator</td><td>Speech Only</td><td>NA</td><td>NA</td>
-        </tr>
-    </table>
+    </tbody>
+</table>
+
 
 
 
