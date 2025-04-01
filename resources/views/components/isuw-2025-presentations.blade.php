c444ecd984d44380f0777b5c7524dbff87edb5f3
Manish <manish@Manishs-Air.attlocal.net>
2025-03-31 21:12:26 -0500

    IWT-158

--- resources/views/components/isuw-2025-presentations.blade.php
+++ resources/views/components/isuw-2025-presentations.blade.php
@@ -10,9 +10,84 @@
 	</div>
 
 
+	<h2>9TH IEC-IEEE-BIS SMART ENERGY STANDARDIZATION COORDINATION WORKSHOP</h2>
+    <table>
+        <tr>
+            <th>S. No</th>
+            <th>Name</th>
+            <th>Designation</th>
+            <th>Organization</th>
+            <th>Category</th>
+            <th>Presentation/Speech</th>
+            <th>Video Link</th>
+            <th>Photo Link</th>
+        </tr>
+        <tr class="session-header">
+            <td colspan="8">Inaugural Session</td>
+        </tr>
+        <tr>
+            <td>1</td><td>Reji Kumar Pillai</td><td>President</td><td>ISGF</td><td>Welcome Address</td><td>Speech Only</td><td>NA</td><td>NA</td>
+        </tr>
+        <tr>
+            <td>2</td><td>Richard Schomberg</td><td>Special Envoy on Smart Electrification</td><td>IEC</td><td>Special Address</td><td>Speech Only</td><td>NA</td><td>NA</td>
+        </tr>
+        <tr>
+            <td>3</td><td>Sreekanth Chandrasekharan</td><td>Senior Director</td><td>IEEE</td><td>Special Address</td><td>Speech Only</td><td>NA</td><td>NA</td>
+        </tr>
+        <tr>
+            <td>4</td><td>BA Sawale</td><td>Director General</td><td>Central Power Research Institute</td><td>Special Address</td><td>Speech Only</td><td>NA</td><td>NA</td>
+        </tr>
+        <tr class="session-header">
+            <td colspan="8">Session 1: Emerging Smart Energy Technologies and Standardization Landscape</td>
+        </tr>
+        <tr>
+            <td>5</td><td>Lonneke Drissen</td><td>Director</td><td>Open Charge Alliance</td><td>Speaker</td><td>View/Download</td><td>NA</td><td>NA</td>
+        </tr>
+        <tr>
+            <td>6</td><td>Neha Agarwal</td><td>Scientist – D / Joint Director</td><td>BIS</td><td>Speaker</td><td>View/Download</td><td>NA</td><td>NA</td>
+        </tr>
+        <tr>
+            <td>7</td><td>Amarjeet Kumar</td><td>Founder & CEO</td><td>Comminent Pvt. Ltd.</td><td>Speaker</td><td>View/Download</td><td>NA</td><td>NA</td>
+        </tr>
+        <tr>
+            <td>8</td><td>Ravindra Desai</td><td>Program Manager – Standards</td><td>IEEE</td><td>Speaker</td><td>View/Download</td><td>NA</td><td>NA</td>
+        </tr>
+        <tr>
+            <td>9</td><td>Remi Demerle</td><td>Marketing director</td><td>LoRA ecosystem</td><td>Speaker</td><td>View/Download</td><td>NA</td><td>NA</td>
+        </tr>
+        <tr>
+            <td>10</td><td>Dinesh Sharma</td><td>Director – Standardization, Policy and Regulation</td><td>SESI</td><td>Speaker</td><td>View/Download</td><td>NA</td><td>NA</td>
+        </tr>
+        <tr class="session-header">
+            <td colspan="8">Session 2: Future Trends and Challenges</td>
+        </tr>
+        <tr>
+            <td>13</td><td>Subhadip Raychaudhuri</td><td>Head-Engineering, Energy Audit, GIS & AMI Applications</td><td>TPDDL</td><td>Speaker</td><td>View/Download</td><td>NA</td><td>NA</td>
+        </tr>
+        <tr>
+            <td>14</td><td>A K Rajput</td><td>Member (Power Systems)</td><td>CEA</td><td>Speaker</td><td>Speech Only</td><td>NA</td><td>NA</td>
+        </tr>
+        <tr>
+            <td>15</td><td>Miguel Martinez</td><td>Senior Business Manager</td><td>UL Solutions</td><td>Speaker</td><td>View/Download</td><td>NA</td><td>NA</td>
+        </tr>
+        <tr>
+            <td>16</td><td>Gopal Nariya</td><td>Vice President</td><td>BRPL</td><td>Speaker</td><td>View/Download</td><td>NA</td><td>NA</td>
+        </tr>
+        <tr>
+            <td>17</td><td>Kishore Narang</td><td>Founder</td><td>Narnix Technolabs Pvt. Ltd.</td><td>Speaker</td><td>Speech Only</td><td>NA</td><td>NA</td>
+        </tr>
+        <tr>
+            <td>18</td><td>Richard Schomberg</td><td>Special Envoy on Smart Electrification</td><td>IEC</td><td>Chair</td><td>Speech Only</td><td>NA</td><td>NA</td>
+        </tr>
+        <tr>
+            <td>19</td><td>N. Murugesan</td><td>Former DG</td><td>CPRI</td><td>Moderator</td><td>Speech Only</td><td>NA</td><td>NA</td>
+        </tr>
+    </table>
 
 
-<table>
+
+
+<!-- <table>
 	<tr>
 		<td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000" colspan=8 height="31" align="center" valign=bottom><b><font size=5 color="#000000">INDIA SMART UTILITY WEEK- ISUW 2024 (12 MARCH TO 16 MARCH 2024)</font></b></td>
 		</tr>
@@ -4215,7 +4290,7 @@
 		<td  align="center" valign=top bgcolor="#D0CECE"><b><font color="#000000">Photo link</font></b></td>
 	</tr>
 	
-</table>
+</table> -->
 
 <p></p>
 </div>
