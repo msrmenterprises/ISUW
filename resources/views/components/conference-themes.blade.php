3c4ed5d81eff543d70198248df6501d6b26e8c9b
Manish <manish@Manishs-Air.attlocal.net>
2025-04-08 22:30:31 -0500

    IWT-188

--- resources/views/components/conference-themes.blade.php
+++ resources/views/components/conference-themes.blade.php
@@ -34,7 +34,7 @@
   <div class="accordion-item">
     <h2 class="accordion-header" id="flush-headingOne">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-         <H4 style="color: #ed7d31;"><strong>EVENT DAY-1: 18 MARCH 2025 (TUESDAY)</strong></style></H4>
+         <H4 style="color: #ed7d31;"><strong>Day 1 – 10 March 2026 (Tuesday)</strong></style></H4>
       </button>
     </h2> 
     <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
@@ -46,27 +46,27 @@ Tools for Text 2 Text (T2T), Audio 2 Text (A2T), Text 2 Audio (T2A), Text 2 Imag
 2 Text (I2T), Text 2 Video (T2V), Text 2 Motion (T2M), Text 2 Code (T2C), Brain 2 Text (B2T),
 Audio 2 Audio (A2A) etc.</li>
 <li>OCPP Technical Workshop and Training (in collaboration with Open Charge Alliance)</li></ul></p>-->
- <p style="color:#ed7d31;">SPECIAL WORKSHOPS
-     <ul><li>OCPP Session (In Collaboration with Open Charge Alliance)</li> 
-<li>RAID Olympiad ( Robots – AI – Drones)</li>
-<li>9th IEC - IEEE Smart Energy Standardization Coordination Workshop (In
-Collaboration with BIS, IEC And IEEE)</li>
-
-</ul></p>
+ <p style="color:#ed7d31;">Special Workshops
+  <!-- <ul><li>OCPP Session (In Collaboration with Open Charge Alliance)</li> 
+  <li>RAID Olympiad ( Robots – AI – Drones)</li>
+  <li>9th IEC - IEEE Smart Energy Standardization Coordination Workshop (In
+  Collaboration with BIS, IEC And IEEE)</li>
+  </ul> -->
+</p>
 </div></div></div></div>
 <p></p>
 <div class="accordion accordion-flush" id="accordionFlushExample">
   <div class="accordion-item">
     <h2 class="accordion-header" id="flush-headingOne">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapsetwo" aria-expanded="false" aria-controls="flush-collapsetwo">
-         <H4 style="color: #ed7d31;"><strong>EVENT DAY- 2: 19 MARCH 2025 (WEDNESDAY)</strong></style></H4>
+         <H4 style="color: #ed7d31;"><strong>Day 2 – 11 March 2026 (Wednesday)</strong></style></H4>
       </button>
     </h2>
     <div id="flush-collapsetwo" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
       <div class="accordion-body">
   <p style="color:#ed7d31;">
       <ul>
-          <li>First Meeting of All India DISCOMs Association (AIDA)</li>
+          <!-- <li>First Meeting of All India DISCOMs Association (AIDA)</li> -->
           <!-- <li>250 Million Smart Meters in India – New Business Opportunities with the
           Digitalization of DISCOMs</li>
           <li>Resiliency of Utilities Against Extreme Weather Events</li>
@@ -74,13 +74,13 @@ Collaboration with BIS, IEC And IEEE)</li>
       </ul>
   </p>
 
-  <p style="color:#ed7d31;">WORKSHOPS/ROUNDTABLES/SEMINARS
-      <ul>
+  <p style="color:#ed7d31;">Conference & Exhibition
+      <!-- <ul>
         <li>UK - India Workshop on ASPIRE Program</li>
         <li>UK - India Workshop on Energy Storage Systems</li>
         <li>1th US - India Smart Energy Workshop (In Collaboration with US Commercial Services, and Underwriters Laboratories) Theme: Standardization</li>
         <li>Africa - India Smart Energy Roundtable</li>
-      </ul>
+      </ul> -->
   </p>
 
 </div></div></div></div>
@@ -89,13 +89,13 @@ Collaboration with BIS, IEC And IEEE)</li>
   <div class="accordion-item">
     <h2 class="accordion-header" id="flush-headingOne">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapsethree" aria-expanded="false" aria-controls="flush-collapsethree">
-         <H4 style="color: #ed7d31;"><strong>EVENT DAY- 3: 20 MARCH 2025 (THURSDAY)</strong></style></H4>
+         <H4 style="color: #ed7d31;"><strong>Day 3 – 12 March 2026 (Thursday)</strong></style></H4>
       </button>
     </h2>
     <div id="flush-collapsethree" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
       <div class="accordion-body">
-        <p style="color:#ed7d31;">CONFERENCE THEMATIC SESSIONS
-          <ul>
+        <p style="color:#ed7d31;">Conference & Exhibition
+          <!-- <ul>
               <li>SMART METERING Part A - Global and Indian Lessons</li>
               <li>SMART METERING Part B - AMISPs and DISCOMs, OEMs, System Integrators</li>
               <li>Rooftop PV Revolution in India and Impact on DISCOMs – National Registry of DERs
@@ -105,15 +105,15 @@ Collaboration with BIS, IEC And IEEE)</li>
               <li>Building Resilient Utility Assets for Congested Cities and Climate Resilience</li>
               <li>Power System Flexibility and DERMS</li>
               <li>Deep Dive Session on AI, ML and Robotics - Use Cases for Utilities</li>
-              </ul>
+              </ul> -->
         </p>
-        <p style="color:#ed7d31;">WORKSHOPS/ROUNDTABLES/SEMINARS
+        <!-- <p style="color:#ed7d31;">WORKSHOPS/ROUNDTABLES/SEMINARS
           <ul>
               <li>Indonesia - India Smart Energy Workshop</li>
               <li>2nd India - Brazil Smart Energy Workshop</li>
               <li>Presentation of Selected Technical Papers</li>
           </ul>
-        </p>
+        </p> -->
 
 </div></div></div></div>
 <p></p>
@@ -121,13 +121,13 @@ Collaboration with BIS, IEC And IEEE)</li>
   <div class="accordion-item">
     <h2 class="accordion-header" id="flush-headingOne">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapsefive" aria-expanded="false" aria-controls="flush-collapsefive">
-         <H4 style="color: #ed7d31;"><strong>EVENT DAY- 4: 21 MARCH 2025 (FRIDAY)</strong></style></H4>
+         <H4 style="color: #ed7d31;"><strong>Day 4 – 13 March 2026 (Friday)</strong></style></H4>
       </button>
     </h2>
     <div id="flush-collapsefive" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
       <div class="accordion-body">
-        <p style="color:#ed7d31;">CONFERENCE THEMATIC SESSIONS
-          <ul>
+        <p style="color:#ed7d31;">Conference & Exhibition | ISGF Innovation Awards Ceremony & Valedictory Function
+          <!-- <ul>
               <li>Digitalization of Utilities, Digitalization Roadmaps; Digital Twins</li>
               <li>Emerging Digital Technologies for Utilities</li>
               <li>Innovations in Regulations for the next round of Power Sector Reforms</li>
@@ -136,13 +136,13 @@ Collaboration with BIS, IEC And IEEE)</li>
               <li>Workshop on Immersive Technologies: AR/VR, Digital Twins, Drones, and Assisted
               Reality for Enhanced Operations and Efficiency</li>
               <li>Evolving Trends in Electric Mobility and V2G Demo</li>
-          </ul>
+          </ul> -->
         </p>
-        <p style="color:#ed7d31;">WORKSHOPS/ROUNDTABLES/SEMINARS
+        <!-- <p style="color:#ed7d31;">WORKSHOPS/ROUNDTABLES/SEMINARS
           <ul>
             <li>Capacity Building in Utilities for Energy Transition</li>
           </ul>
-        </p>
+        </p> -->
 <p></p>
 <!-- <p><b>ISGF Innovation Awards Ceremony And Gala Dinner</b></p> -->
 </div></div></div></div>
@@ -151,26 +151,26 @@ Collaboration with BIS, IEC And IEEE)</li>
   <div class="accordion-item">
     <h2 class="accordion-header" id="flush-headingOne">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapsesix" aria-expanded="false" aria-controls="flush-collapsesix">
-         <H4 style="color: #ed7d31;"><strong>EVENT DAY- 5: 22 MARCH 2025 (SATURDAY)</strong></style></H4>
+         <H4 style="color: #ed7d31;"><strong>Day 5 – 14 March 2026 (Saturday)</strong></style></H4>
       </button>
     </h2>
     <div id="flush-collapsesix" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
       <div class="accordion-body">
 <p></p>
-      <p style="color:#ed7d31;">DEMONSTRATIONS, TECHNICAL AND CULTURAL TOURS</p>
-        <ul>
+      <p style="color:#ed7d31;">Technical Tours and Cultural Tours</p>
+        <!-- <ul>
           <li>TOUR 1A - NRLDC+REMC and TPDDL LAB + 10 MW BESS</li>
           <li>TOUR 1B - TPDDL LAB + 10 MW BESS and NRLDC+REMC</li>
           <li>TOUR 2 - 800 kV HVDC STATION IN AGRA + TAJ MAHAL</li>
-        </ul>
+        </ul> -->
       </p>
-<p></p>
+<!-- <p></p>
       <p style="color:#ed7d31;">CULTURAL TOURS (ALL DAY)</p>
         <ul>
           <li>TOUR 3 – TAJ MAHAL</li>
           <li>TOUR 4 - DELH</li>
-        </ul>
-      <p>      
+        </ul> 
+      <p>--> 
 <!-- <p></p>
 <p><b>Disclaimer : Tours 1, 2 &amp; 3 are part of ISUW Package; Tours 4&amp;5 are to be paid separately to the
 Travel Agency | for booking technical &amp; Cultural tours visit the registration desk at Hotel during
@@ -196,7 +196,7 @@ Travel Agency | for booking technical &amp; Cultural tours visit the registratio
 ISUW 2023 Detailed Agenda and Program:</B></p>
 <div class="register-now">
 					 <a href="http://www.isuw.in/conference-agenda-and-program-2023/" class="button" target="_blank">Visit for Agenda</a>
-					 <!--<a href="https://isuw.in/votenowisgf/" class="button" target="_blank">VOTE FOR ISGF INNOVATION AWARDS</a>
+					 <a href="https://isuw.in/votenowisgf/" class="button" target="_blank">VOTE FOR ISGF INNOVATION AWARDS</a>
 					 </div>-->
 
 
