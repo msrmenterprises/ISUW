da19ad07e1968db5a54eacb39cb1edbb45acce2a
Manish <manish@Manishs-Air.attlocal.net>
2025-04-08 22:11:06 -0500

    IWT-166

--- resources/views/components/conference-agenda-and-program-2025.blade.php
+++ resources/views/components/conference-agenda-and-program-2025.blade.php
@@ -9,7 +9,7 @@
 		<h1>ISUW 2026<span class="k_greencolor"> Agenda</span> </h1>
 		<img src="/public/uploads/images/kborder_bottom.png" alt=""> 
 	</div>
-	<p> ISUW 2026 Agenda will be updated closer to the event date. For Agenda details, please write to us at <a href="mailto:dum@indiasmartgrid.org">dum@indiasmartgrid.org</a></p>
+	<p> ISUW 2026 Agenda will be updated closer to the event date. For Agenda details, please write to us at <a href="mailto:isuw@isuw.in">isuw@isuw.in</a></p>
 <!--<h4 style="color: green;margin-left: 58px;"><a href="https://events.firstviewgroup.com/AfricaRooftopSolarCongress2021#/agenda?lang=en" target="_blank">For Agenda Click here </a></h4>-->
 
 <!-- <p style="text-align:center; font-size:16px;"><a href="images/2025/ISUW2025agenda.pdf" target="_blank" class="getstartedd ">Download Agenda</a></p>
