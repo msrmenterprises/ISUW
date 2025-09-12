094b790e2cfd3e815653b8f275fc0d214be34511
Manish <manish@mac.mynetworksettings.com>
2025-09-11 22:12:06 -0500

    fgina

--- 2025/resources/views/components/header.blade.php
+++ 2025/resources/views/components/header.blade.php
@@ -35,53 +35,53 @@
          </button>
          <div class="collapse navbar-collapse navbar" id="navbarSupportedContent">
             <ul class="">
-               <li><a class="nav-link scrollto active ps-0" href="/">HOME</a></li>
+               <li><a class="nav-link scrollto active ps-0" href="./">HOME</a></li>
 			    <li class="dropdown">
                   <a href="/isuw-2021" class=" " data-bs-toggle="dropdown"><span>ABOUT ISUW</span> <i class="fa fa-chevron-down"></i></a>
                   <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-				     <li><a href="/introduction" target="_blank">Introduction</a></li>
-					 <li><a href="/key-highlights" target="_blank">Key Highlights</a></li>
-					 <li><a href="/participating-countries" target="_blank">Participating Countries</a></li>
-					 <li><a href="/isuw-2025-brochure" target="_blank">Brochure</a></li>
-                     <li><a href="/about-organizer" target="_blank">About Organizer</a></li> 
+				     <li><a href="./introduction" target="_blank">Introduction</a></li>
+					 <li><a href="./key-highlights" target="_blank">Key Highlights</a></li>
+					 <li><a href="./participating-countries" target="_blank">Participating Countries</a></li>
+					 <li><a href="./isuw-2025-brochure" target="_blank">Brochure</a></li>
+                     <li><a href="./about-organizer" target="_blank">About Organizer</a></li> 
 					  <!--<li><a href="/isuw-2021">ISUW 2021</a></li>-->
-					  <li><a href="/isgw-post-event-report" target="_blank">Last Edition Post Event Report</a></li>
+					  <li><a href="./isgw-post-event-report" target="_blank">Last Edition Post Event Report</a></li>
                   </ul>
                </li>
 			     <li class="dropdown">
                   <a href="#" class=" " data-bs-toggle="dropdown"><span>PROGRAM</span> <i class="fa fa-chevron-down"></i></a>
                   <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                     <li><a href="/conference-themes" target="_blank">ISUW Conference Program and Themes</a></li> 
-                      <li><a href="/conference-agenda-and-program-2025" target="_blank">Conference Agenda and Program</a></li>
-                      <li><a href="/technical-paper-isuw" target="_blank">ISUW Technical Papers</a></li>
-                      <li><a href="/innovation-awards" target="_blank">Innovation Awards</a></li>
-                       <li><a href="/technical-tours" target="_blank">Technical Tours </a></li>
+                     <li><a href="./conference-themes" target="_blank">ISUW Conference Program and Themes</a></li> 
+                      <li><a href="./conference-agenda-and-program-2025" target="_blank">Conference Agenda and Program</a></li>
+                      <li><a href="./technical-paper-isuw" target="_blank">ISUW Technical Papers</a></li>
+                      <li><a href="./innovation-awards" target="_blank">Innovation Awards</a></li>
+                       <li><a href="./technical-tours" target="_blank">Technical Tours </a></li>
                        <!-- <li><a href="/master-classes" target="_blank">Master Classes</a></li> -->
-                       <li><a href="/raid-olympiad" target="_blank">Raid Olympiad</a></li>
-                       <li><a href="/cultural-tours" target="_blank">Cultural Tours</a></li>
+                       <li><a href="./raid-olympiad" target="_blank">Raid Olympiad</a></li>
+                       <li><a href="./cultural-tours" target="_blank">Cultural Tours</a></li>
                   </ul>
                </li>
                <li class="dropdown">
                   <a href="#" class=" " data-bs-toggle="dropdown"><span>PARTNERS</span> <i class="fa fa-chevron-down"></i></a>
                   <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                     <li><a href="/partnership-opportunity-information" target="_blank">Partnership Opportunities</a></li>
-                      <li><a href="/key-partners" target="_blank">Key Partners</a></li>
-                      <li><a href="/supporting-organization" target="_blank">Supporting Organizations</a></li>
-                      <li><a href="/supporting-ministries-2025" target="_blank">Supporting Ministries</a></li>
-                      <li><a href="/media-and-marketing-partner-2024" target="_blank">Media and Marketing Partners</a></li>
-                      <li><a href="/theme-and-session-partners-2025" target="_blank">Conference Sessions Partners</a></li>
+                     <li><a href="./partnership-opportunity-information" target="_blank">Partnership Opportunities</a></li>
+                      <li><a href="./key-partners" target="_blank">Key Partners</a></li>
+                      <li><a href="./supporting-organization" target="_blank">Supporting Organizations</a></li>
+                      <li><a href="./supporting-ministries-2025" target="_blank">Supporting Ministries</a></li>
+                      <li><a href="./media-and-marketing-partner-2024" target="_blank">Media and Marketing Partners</a></li>
+                      <li><a href="./theme-and-session-partners-2025" target="_blank">Conference Sessions Partners</a></li>
                       <!--<li><a href="/supporting-utilities-of-isuw" target="_blank">Supporting Utilities</a></li>-->
-                     <li><a href="/participating-utilities-2025" target="_blank">Participating Utilities</a></li>
-                     <li><a href="/isuw-participated-utilties" target="_blank">Participated Utilities</a></li>
+                     <li><a href="./participating-utilities-2025" target="_blank">Participating Utilities</a></li>
+                     <li><a href="./isuw-participated-utilties" target="_blank">Participated Utilities</a></li>
                   </ul>
                </li>
 			 <li class="dropdown">
                   <a href="#" class=" " data-bs-toggle="dropdown"><span>EXHIBITION</span> <i class="fa fa-chevron-down"></i></a>
                   <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                     <li><a href="/exhibition-themes" target="_blank">Exhibition Themes</a></li>
-                      <li><a href="/exhibition-package-2024" target="_blank">Exhibition Packages</a></li>
-                      <li><a href="/exhibition-gallery" target="_blank">Exhibition Gallery</a></li>
-                      <li><a href="/confirmed-exhibitors-2024" target="_blank">Confirmed Exhibitors</a></li>
+                     <li><a href="./exhibition-themes" target="_blank">Exhibition Themes</a></li>
+                      <li><a href="./exhibition-package-2024" target="_blank">Exhibition Packages</a></li>
+                      <li><a href="./exhibition-gallery" target="_blank">Exhibition Gallery</a></li>
+                      <li><a href="./confirmed-exhibitors-2024" target="_blank">Confirmed Exhibitors</a></li>
                   </ul>
                </li>
               
@@ -91,19 +91,19 @@
 				   <li class="dropdown">
                   <a href="#" class=" " data-bs-toggle="dropdown"><span>MEDIA</span> <i class="fa fa-chevron-down"></i></a>
                   <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                     <li><a href="/photo-gallery" target="_blank">Photos</a></li>
-					  <li><a href="/video-gallery" target="_blank">Videos</a></li>
-					   <li><a href="/ISUW-in-media" target="_blank">Media Coverage</a></li>
-					    <li><a href="/press-release" target="_blank">Press Release</a></li>
-					    <li><a href="/podcast" target="_blank">ISUW Podcast</a></li>
+                     <li><a href="./photo-gallery" target="_blank">Photos</a></li>
+					  <li><a href="./video-gallery" target="_blank">Videos</a></li>
+					   <li><a href="./ISUW-in-media" target="_blank">Media Coverage</a></li>
+					    <li><a href="./press-release" target="_blank">Press Release</a></li>
+					    <li><a href="./podcast" target="_blank">ISUW Podcast</a></li>
                   </ul>
                </li>
                
 			   <li class="dropdown">
                   <a href="#" class=" " data-bs-toggle="dropdown"><span>CONTACT US</span> <i class="fa fa-chevron-down"></i></a>
                   <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                     <li><a href="/contact-us" target="_blank">Contact us</a></li>
-					 <li><a href="/enquiry" target="_blank">Enquiry</a></li>
+                     <li><a href="./contact-us" target="_blank">Contact us</a></li>
+					 <li><a href="./enquiry" target="_blank">Enquiry</a></li>
                   </ul>
                </li>
                <li><a class="getstarted scrollto" href="register">Register Now</a></li>
