195c9bd7f8fdbdcce5560f96645d99bb8c7009f3
msrmenterprises <connect@msrmenterprises.com>
2025-12-18 21:09:09 -0600

    Update webasia.php

--- resources/views/components/webasia.php
+++ resources/views/components/webasia.php
@@ -1,4 +1,4 @@
-@extends('master')
+<!-- @extends('master')
 @section('content')
 
 <div class="container">
@@ -6,7 +6,7 @@
         <p></p>
         
 
-        <!-- WE3 ASIA 2026 部分 -->
+        
         <div class="k_cheading">
             <h1>WE3 ASIA <span class="k_greencolor">2026</span></h1>
             <h4 style="text-align:center;">09 – 10 MARCH 2026</h4>
@@ -63,7 +63,7 @@
             </div>
         </div>
 
-        <!-- ISUW 2026 Technical Tour 部分（保留原结构） -->
+       
         <div class="k_cheading">
             <h1>ISUW 2026 <span class="k_greencolor">Technical Tour</span></h1>
             <h4 style="text-align:center;">14th March 2026</h4>
@@ -111,6 +111,80 @@
         <p></p>
 
     </div>
+</div>
+
+@endsection -->
+
+
+@extends('master')
+@section('content')
+
+ <div class="container">
+ <div class="row justify-content-left ">
+<p></p>
+<style>
+  
+</style> 
+ <div class="k_cheading">
+		<h1>ISUW 2026 <span class="k_greencolor">Technical Tour</span> </h1>
+		<h4 style="text-align:center;">14th March 2026</h4>
+		<img src="/public/uploads/images/kborder_bottom.png" alt=""> 
+	</div>
+	<div class="accordion accordion-flush" id="accordionFlushExample">
+<p></p>
+ <div class="accordion-item">
+    <h2 class="accordion-header" id="flush-headingOne">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapsewinners-2" aria-expanded="false" aria-controls="flush-collapsewinners-2">
+       <H4 style="color: green;"><strong>14th March 2026 (Saturday) - ISUW Technical Tour  </strong></style></H4>
+      </button>
+    </h2>
+    <div id="flush-collapsewinners-2" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+      <div class="accordion-body">
+        <table border="1" cellpadding="10" cellspacing="0">
+      <tr>
+      <th>Tour</th>
+      <th>Description</th>
+      </tr>
+      <tr>
+      <td>Tour 1</td>
+      <td>Northern Regional Load Dispatch Centre(NRLDC) and REMC and Tata Power DDL Smart Grid Lab and Battery Energy Storage System(BESS), New Delhi*</td>
+      </tr>
+      <tr>
+      <td>Tour 2</td>
+      <td>800 kV HVDC Station IN AGRA + TAJ MAHAL</td>
+      </tr>
+      
+      
+      </table><br/>
+      <p>Please Note 
+        <ul style="margin-left:21px">
+            <li>Tour Departure and Arrival will be from ISUW 2026 Venue</li>
+
+            <li>Passport and Visa Copy/ Aadhar Card Mandatory</li>
+
+            <li>Advance Registration Mandatory for ISUW Speakers, Partners, Exhibitors and Delegates with Technical Tour </li>
+
+            <li>Technical Tour return timing is subject to traffic. Please book your flights at least 4-5 hours after mentioned time</li>
+
+            <li>Program Under Finalization*</li>
+
+          </ul>
+      </p>
+
+
+    </div>
+  </div>
+</div></div>
+<p></p>
+
+<p></p>
+<p></p>
+
+
+
+<p></p>
+<p></p>
+</div>
 </div>
 
 @endsection
