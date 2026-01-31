86d76cd8a87438b6d8153f8809d29a71424e2590
msrmenterprises <connect@msrmenterprises.com>
2026-01-30 20:03:42 -0600

    Update home.blade.php

--- resources/views/components/home.blade.php
+++ resources/views/components/home.blade.php
@@ -1341,7 +1341,7 @@
                   <div class="sprk-box">
                      <img src="{{$speaker24->imageUrl}}" class="img-fluid " alt="...">
                      <h5>{{$speaker24->name}}</h5>
-                     <p>{{$speaker24->company}}</p>
+                     <p>{!! nl2br(e($speaker24->company)) !!}</p>
                      <p><a href="speaker24/{{$speaker24->id}}" class="text-warning">Read bio...</a></p>
                   </div>
                   @endif
