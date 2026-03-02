66a3095caa8114fb375e0ad29241e5dbe5d96d9c
msrmenterprises <connect@msrmenterprises.com>
2026-03-01 22:39:44 -0600

    Update theme-and-session-partners-2025.blade.php

--- resources/views/components/theme-and-session-partners-2025.blade.php
+++ resources/views/components/theme-and-session-partners-2025.blade.php
@@ -154,29 +154,97 @@
 
 
 <div class="k_cheading">
-		<h1>ISUW 2025 <span class="k_greencolor">Conference Sessions Partner</span> </h1>
-		<img src="/public/uploads/images/kborder_bottom.png" alt=""> 
+	<h1>ISUW 2025 <span class="k_greencolor">Conference Sessions Partner</span> </h1>
+	<img src="/public/uploads/images/kborder_bottom.png" alt=""> 
+</div>
+<div class="partners-grid grid-4">
+	<div class="logo-card">
+		<a href="https://www.aspentech.com/en" target="_blank" rel="noopener noreferrer">
+			<img src="images/2025/AspenTech.png" alt="AspenTech">
+		</a>
 	</div>
-	<div class="col-lg-4"><div class="logo-div"><a href="https://www.aspentech.com/en" target="_blank" rel="noopener noreferrer"><img src="images/2025/AspenTech.png" alt=""  height="102" style="display:inline;"></div></a></div>
-	<div class="col-lg-4"><div class="logo-div"><a href="#" target="_blank" rel="noopener noreferrer"><img src="images/2025/BSCE.png" alt=""  height="102" style="display:inline;"></div></a></div>
-	
-	<div class="col-lg-4"><div class="logo-div"><a href="https://www.ideasmattermost.com/" target="_blank" rel="noopener noreferrer"><img class="aligncenter size-full wp-image-5610" src="images/2025/Ideasmatter.png" alt="TIE"></a></div></div>
-	<div class="col-lg-4"><div class="logo-div"><a href="https://www.iec.ch/homepage" target="_blank" rel="noopener noreferrer"><img class="aligncenter size-full wp-image-5610" src="images/2025/IEC.png" alt="TIE"></a></div></div>
-	<div class="col-lg-4"><div class="logo-div"><a href="https://www.ieee.org/" target="_blank" rel="noopener noreferrer"><img class="aligncenter size-full wp-image-5610" src="images/2025/IEEE.png" alt="TIE"></a></div></div>
-	<div class="col-lg-4"><div class="logo-div"><a href="https://apua-asea.org/en/" target="_blank" rel="noopener noreferrer"><img class="aligncenter size-full wp-image-5610" src="images/2025/APUA.jpg" alt="TIE"></a></div></div>
-	<div class="col-lg-4"><div class="logo-div"><a href="https://openchargealliance.org/" target="_blank" rel="noopener noreferrer"><img class="aligncenter size-full wp-image-5610" src="images/2025/OpenChargeAlliance.jpg" alt="TIE"></a></div></div>
-	<div class="col-lg-4"><div class="logo-div"><a href="https://www.trade.gov/us-commercial-service" target="_blank" rel="noopener noreferrer"><img class="aligncenter size-full wp-image-5610" src="images/2025/UScommercial.png" alt="TIE"></a></div></div>
+	<div class="logo-card">
+		<a href="#" target="_blank" rel="noopener noreferrer">
+			<img src="images/2025/BSCE.png" alt="BSCE">
+		</a>
+	</div>
+	<div class="logo-card">
+		<a href="https://www.ideasmattermost.com/" target="_blank" rel="noopener noreferrer">
+			<img src="images/2025/Ideasmatter.png" alt="Ideasmatter">
+		</a>
+	</div>
+	<div class="logo-card">
+		<a href="https://www.iec.ch/homepage" target="_blank" rel="noopener noreferrer">
+			<img src="images/2025/IEC.png" alt="IEC">
+		</a>
+	</div>
+	<div class="logo-card">
+		<a href="https://www.ieee.org/" target="_blank" rel="noopener noreferrer">
+			<img src="images/2025/IEEE.png" alt="IEEE">
+		</a>
+	</div>
+	<div class="logo-card">
+		<a href="https://apua-asea.org/en/" target="_blank" rel="noopener noreferrer">
+			<img src="images/2025/APUA.jpg" alt="APUA">
+		</a>
+	</div>
+	<div class="logo-card">
+		<a href="https://openchargealliance.org/" target="_blank" rel="noopener noreferrer">
+			<img src="images/2025/OpenChargeAlliance.jpg" alt="Open Charge Alliance">
+		</a>
+	</div>
+	<div class="logo-card">
+		<a href="https://www.trade.gov/us-commercial-service" target="_blank" rel="noopener noreferrer">
+			<img src="images/2025/UScommercial.png" alt="US Commercial">
+		</a>
+	</div>
+	<div class="logo-card">
+		<a href="https://aida-india.org/" target="_blank" rel="noopener noreferrer">
+			<img src="public/uploads/images/2026/AIDA.png" alt="AIDA">
+		</a>
+	</div>
+	<div class="logo-card">
+		<a href="https://www.bis.gov.in/" target="_blank" rel="noopener noreferrer">
+			<img src="images/2025/BIS.jpg" alt="BIS">
+		</a>
+	</div>
+	<div class="logo-card">
+		<a href="https://www.cdri.world/" target="_blank" rel="noopener noreferrer">
+			<img src="images/2025/CDRI.jpg" alt="CDRI">
+		</a>
+	</div>
+	<div class="logo-card">
+		<a href="https://www.indiabrazilchamber.org/home" target="_blank" rel="noopener noreferrer">
+			<img src="images/2025/IBCoC.png" alt="India Brazil Chamber of Commerce">
+		</a>
+	</div>
+	<div class="logo-card">
+		<a href="https://www.smartgrid.com.br/" target="_blank" rel="noopener noreferrer">
+			<img src="images/2025/LATAM.png" alt="LATAM">
+		</a>
+	</div>
+	<div class="logo-card">
+		<a href="https://sscgj.in/" target="_blank" rel="noopener noreferrer">
+			<img src="images/2025/SCGJ.png" alt="SCGJ">
+		</a>
+	</div>
+	<div class="logo-card">
+		<a href="https://www.gov.uk/government/organisations/foreign-commonwealth-development-office" target="_blank" rel="noopener noreferrer">
+			<img src="images/2025/UKGovSTACKRGB.png" alt="UK Government">
+		</a>
+	</div>
+	<div class="logo-card">
+		<a href="https://pjci.idremember.com/" target="_blank" rel="noopener noreferrer">
+			<img src="https://pjci.idremember.com/wp-content/uploads/2022/05/download__1_-removebg-preview.png" alt="PJCI">
+		</a>
+	</div>
+	<div class="logo-card">
+		<a href="https://ulse.org/" target="_blank" rel="noopener noreferrer">
+			<img src="images/2025/ulstandengage.svg" alt="ULSE">
+		</a>
+	</div>
+</div>
 
-	<div class="col-lg-4"><div class="logo-div"><a href="https://aida-india.org/" target="_blank" rel="noopener noreferrer"><img class="aligncenter size-full wp-image-5610" src="public/uploads/images/2026/AIDA.png" alt="TIE"></a></div></div>
-	<div class="col-lg-4"><div class="logo-div"><a href="https://www.bis.gov.in/" target="_blank" rel="noopener noreferrer"><img class="aligncenter size-full wp-image-5610" src="images/2025/BIS.jpg" alt="TIE"></a></div></div>
-	<div class="col-lg-4"><div class="logo-div"><a href="https://www.cdri.world/" target="_blank" rel="noopener noreferrer"><img class="aligncenter size-full wp-image-5610" src="images/2025/CDRI.jpg" alt="TIE"></a></div></div>
-	<div class="col-lg-4"><div class="logo-div"><a href="https://www.indiabrazilchamber.org/home" target="_blank" rel="noopener noreferrer"><img class="aligncenter size-full wp-image-5610" src="images/2025/IBCoC.png" alt="TIE"></a></div></div>
-	<div class="col-lg-4"><div class="logo-div"><a href="https://www.smartgrid.com.br/" target="_blank" rel="noopener noreferrer"><img class="aligncenter size-full wp-image-5610" src="images/2025/LATAM.png" alt="TIE"></a></div></div>
-	<div class="col-lg-4"><div class="logo-div"><a href="https://sscgj.in/" target="_blank" rel="noopener noreferrer"><img class="aligncenter size-full wp-image-5610" src="images/2025/SCGJ.png" alt="TIE"></a></div></div>
-	<div class="col-lg-4"><div class="logo-div"><a href="https://www.gov.uk/government/organisations/foreign-commonwealth-development-office" target="_blank" rel="noopener noreferrer"><img class="aligncenter size-full wp-image-5610" src="images/2025/UKGovSTACKRGB.png" alt="TIE"></a></div></div>
-	<div class="col-lg-4"><div class="logo-div"><a href="https://pjci.idremember.com/" target="_blank" rel="noopener noreferrer"><img class="aligncenter size-full wp-image-5610" src="https://pjci.idremember.com/wp-content/uploads/2022/05/download__1_-removebg-preview.png" alt="TIE"></a></div></div>
-	<div class="col-lg-4"><div class="logo-div"><a href="https://ulse.org/" target="_blank" rel="noopener noreferrer"><img class="aligncenter size-full wp-image-5610" src="images/2025/ulstandengage.svg" alt="TIE"></a></div></div>
-	
  
 
 <!-- <h5 style="text-align:center;"><b>Session Partners</b></h5>
