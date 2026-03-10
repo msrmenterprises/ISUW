d17ed214e7c90176804c3da9c106966e86f6f58c
msrmenterprises <connect@msrmenterprises.com>
2026-03-09 20:47:18 -0500

    Update participating-utilities-2025.blade.php

--- resources/views/components/participating-utilities-2025.blade.php
+++ resources/views/components/participating-utilities-2025.blade.php
@@ -5,6 +5,48 @@
  <div class="row justify-content-left ">
 <p></p>
 
+	 <div class="k_cheading">
+		<h1>ISUW 2026 <span class="k_greencolor">Participating Utilities </span> </h1>
+		<img src="/public/uploads/images/kborder_bottom.png" alt="">
+	</div>
+<p></p>
+<div class="col-lg-3"><div class="logo-div"><a href="https://www.bsesdelhi.com/web/brpl" target="_blank" rel="noopener noreferrer"><img src="https://www.isuw.in/public/uploads/utility/1641110129.png" alt="BSES Rajdhani Power Limited"></a></div></div>
+<div class="col-lg-3"><div class="logo-div"><a href="https://www.bsesdelhi.com/web/bypl" target="_blank" rel="noopener noreferrer"><img src="https://www.isuw.in/public/uploads/utility/1641110101.png" alt="BSES Yamuna Power Limited"></a></div></div>
+<div class="col-lg-3"><div class="logo-div"><a href="http://www.tatapower-ddl.com/" target="_blank" rel="noopener noreferrer"><img src="https://www.isuw.in/public/uploads/utility/1641110211.png" alt="Tata Power DDL"></a></div></div>
+<div class="col-lg-3"><div class="logo-div"><a href="https://www.tatapower.com/" target="_blank" rel="noopener noreferrer"><img src="https://www.isuw.in/public/uploads/utility/1641110147.png" alt="Tata Power Company Limited"></a></div></div>
+<div class="col-lg-3"><div class="logo-div"><a href="https://www.noidapower.com/" target="_blank" rel="noopener noreferrer"><img src="images/2026/NPCL_logo.png" alt="NPCL"></a></div></div>
+<div class="col-lg-3"><div class="logo-div"><a href="https://www.powergrid.in/en/" target="_blank" rel="noopener noreferrer"><img src="images/2025/Powergrid.png" alt="Powergrid"></a></div></div>
+<div class="col-lg-3"><div class="logo-div"><a href="https://grid-india.in/en/" target="_blank" rel="noopener noreferrer"><img src="images/2025/Grid-India.png" alt="Grid India"></a></div></div>
+<div class="col-lg-3"><div class="logo-div"><a href="https://kseb.in/" target="_blank" rel="noopener noreferrer"><img src="images/2025/KSEB.png" alt="KSEB"></a></div></div>
+<div class="col-lg-3"><div class="logo-div"><a href="https://pgvcl.com/" target="_blank" rel="noopener noreferrer"><img src="images/2025/PGVCL.jpg" alt="PGVCL"></a></div></div>
+<div class="col-lg-3"><div class="logo-div"><a href="https://pedservices.py.gov.in/" target="_blank" rel="noopener noreferrer"><img src="public/uploads/utility/Puducherry.jpg" alt="Puducherry Electricity Department"></a></div></div>
+<div class="col-lg-3"><div class="logo-div"><a href="https://www.tpnodl.com/" target="_blank" rel="noopener noreferrer"><img src="images/2025/TPNODL.png" alt="TPNODL"></a></div></div>
+<div class="col-lg-3"><div class="logo-div"><a href="https://www.tpwesternodisha.com/" target="_blank" rel="noopener noreferrer"><img src="images/2025/TPWODL.png" alt="TPOWDL"></a></div></div>
+<div class="col-lg-3"><div class="logo-div"><a href="https://www.tpcentralodisha.com/" target="_blank" rel="noopener noreferrer"><img src="images/2025/TPCODL.png" alt="TPCODL"></a></div></div>
+<div class="col-lg-3"><div class="logo-div"><a href="https://www.tpsouthernodisha.com/" target="_blank" rel="noopener noreferrer"><img src="images/2025/TPSODL.png" alt="TPSODL"></a></div></div>
+<div class="col-lg-3"><div class="logo-div"><a href="https://www.apcpdcl.in/" target="_blank" rel="noopener noreferrer"><img src="https://logo.clearbit.com/apcpdcl.in" alt="APCPDCL"></a></div></div>
+<div class="col-lg-3"><div class="logo-div"><a href="https://www.cesc.co.in/" target="_blank" rel="noopener noreferrer"><img src="images/2025/CESC_Logo.png" alt="CESC Limited"></a></div></div>
+<div class="col-lg-3"><div class="logo-div"><a href="https://uppcl.org/uppcl/en" target="_blank" rel="noopener noreferrer"><img src="images/2025/UPPCL.png" alt="UPPCL"></a></div></div>
+<div class="col-lg-3"><div class="logo-div"><a href="https://www.guvnl.com/" target="_blank" rel="noopener noreferrer"><img src="images/2025/GUVNL.png" alt="GUVNL"></a></div></div>
+<div class="col-lg-3"><div class="logo-div"><a href="https://apdcl.org/website/" target="_blank" rel="noopener noreferrer"><img src="https://logo.clearbit.com/apdcl.org" alt="APDCL"></a></div></div>
+<div class="col-lg-3"><div class="logo-div"><a href="https://kesco.co.in/Default" target="_blank" rel="noopener noreferrer"><img src="public/uploads/utility/KESCO.png" alt="KESCo"></a></div></div>
+<div class="col-lg-3"><div class="logo-div"><a href="https://energy.rajasthan.gov.in/content/raj/energy-department/jdvvnl/en/home.html" target="_blank" rel="noopener noreferrer"><img src="public/uploads/utility/JdVVNLa.jpg" alt="JDVVNL"></a></div></div>
+<div class="col-lg-3"><div class="logo-div"><a href="https://puvvnl.up.nic.in/" target="_blank" rel="noopener noreferrer"><img src="public/uploads/utility/PUVVNL.jpeg" alt="PUVVNL"></a></div></div>
+<div class="col-lg-3"><div class="logo-div"><a href="https://www.mahadiscom.in/en/home/" target="_blank" rel="noopener noreferrer"><img src="images/2025/MSEDCL.png" alt="MSEDCL"></a></div></div>
+<div class="col-lg-3"><div class="logo-div"><a href="https://energy.rajasthan.gov.in/content/raj/energy-department/avvnl/en/home.html" target="_blank" rel="noopener noreferrer"><img src="https://logo.clearbit.com/energy.rajasthan.gov.in" alt="AVVNL"></a></div></div>
+<div class="col-lg-3"><div class="logo-div"><a href="https://www.mvvnl.in/" target="_blank" rel="noopener noreferrer"><img src="public/uploads/utility/Madhyanchal Vidyut Vitran Nigam Ltd.png" alt="MVVNL"></a></div></div>
+<div class="col-lg-3"><div class="logo-div"><a href="https://www.nbpdcl.co.in/" target="_blank" rel="noopener noreferrer"><img src="public/uploads/utility/North Bihar Power Distribution Company Limited.jpg" alt="NBEDCL"></a></div></div>
+<div class="col-lg-3"><div class="logo-div"><a href="https://www.torrentpower.com/" target="_blank" rel="noopener noreferrer"><img src="public/uploads/utility/Torrent Power Ltd.png" alt="Torrent Power"></a></div></div>
+<div class="col-lg-3"><div class="logo-div"><a href="https://energy.rajasthan.gov.in/content/raj/energy-department/jvvnl/en/home.html" target="_blank" rel="noopener noreferrer"><img src="https://logo.clearbit.com/energy.rajasthan.gov.in" alt="JVVNL"></a></div></div>
+<div class="col-lg-3"><div class="logo-div"><a href="https://www.wbsedcl.in/" target="_blank" rel="noopener noreferrer"><img src="public/uploads/utility/WBSEDCL.png" alt="WBSEDCL"></a></div></div>
+<div class="col-lg-3"><div class="logo-div"><a href="https://www.adanielectricity.com/" target="_blank" rel="noopener noreferrer"><img src="public/uploads/utility/adani.jpg" alt="Adani Electricity"></a></div></div>
+<div class="col-lg-3"><div class="logo-div"><a href="https://www.pvvnl.org/" target="_blank" rel="noopener noreferrer"><img src="images/2025/PVVNL-Logo.png" alt="PVVNL"></a></div></div>
+<div class="col-lg-3"><div class="logo-div"><a href="https://gescomglb.org/gescom/main/home" target="_blank" rel="noopener noreferrer"><img src="public/uploads/utility/GESCOM.jpg" alt="GESCOM"></a></div></div>
+<div class="col-lg-3"><div class="logo-div"><a href="https://dvc.gov.in/" target="_blank" rel="noopener noreferrer"><img src="images/2026/DVCLogo.png" alt="DVC"></a></div></div>
+<div class="col-lg-3"><div class="logo-div"><a href="https://www.apspdcl.in/index.jsp" target="_blank" rel="noopener noreferrer"><img src="images/2025/APSPDCL.png" alt="APSPDCL"></a></div></div>
+<div class="col-lg-3"><div class="logo-div"><a href="https://www.mahatransco.in/" target="_blank" rel="noopener noreferrer"><img src="https://logo.clearbit.com/mahatransco.in" alt="MSETCL"></a></div></div>
+<p></p>
+
 
 	 <div class="k_cheading">
 		<h1>ISUW 2025 <span class="k_greencolor">Participating Utilities </span> </h1>
@@ -28,7 +70,7 @@
 
 <div class="col-lg-3">
 	<div class="logo-div">
-		<a href="https://www.tpwesternodisha.com/" target="_blank" rel="noopener noreferrer">
+		<a href="https://www.tpcentralodisha.com/" target="_blank" rel="noopener noreferrer">
 			<img class="alignleft size-medium wp-image-38051"  src="images/2025/TPCODL.png" alt="" height="125" /> 
 		</a>
 	</div>
@@ -206,7 +248,7 @@
 
 <div class="col-lg-3">
 	<div class="logo-div">
-		<a href="https://posoco.in/en/" target="_blank" rel="noopener noreferrer">
+		<a href="https://grid-india.in/en/" target="_blank" rel="noopener noreferrer">
 			<img class="alignleft size-medium wp-image-38051"  src="images/2025/Grid-India.png" alt="" height="125" /> 
 		</a>
 	</div>
